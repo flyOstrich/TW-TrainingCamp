60ace1c8a39e76411771bad7620b04d1dd41bd95
jianlun.peng <jianlun.peng@thoughtworks.com>
2019-06-01 15:58:52 +0800

    add signin page

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,8 +3,9 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import ElementUI from "element-ui";
-Vue.use(ElementUI);
+import "element-ui/lib/theme-chalk/index.css";
 Vue.config.productionTip = false;
+Vue.use(ElementUI);
 
 new Vue({
   router,
---
+++ frontend/src/views/SignIn.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="signin">
+    <el-form :model="form">
+      <el-form-item label="用户名">
+        <el-button slot="prepend" icon="el-icon-search"></el-button>
+        <el-input v-model="form.username" placeholder="请输入用户名"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-button slot="prepend" icon="el-icon-search"></el-button>
+        <el-input type="password" v-model="form.password" placeholder="请输入密码"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary">登录</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        username: "",
+        password: ""
+      }
+    };
+  }
+};
+</script>
+<style lang="less">
+.signin {
+  width: 40vw;
+  min-width: 500px;
+  margin: 200px auto;
+}
+</style>
