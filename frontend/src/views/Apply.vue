348f40a14f7d26cbe677b7a05f15285fbcae875c
Cheng li <cheng.li1@CNcheng.li1>
2019-06-01 16:03:25 +0800

    .

---
+++ frontend/src/views/Apply.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="apply">
+    <el-divider content-position="center">语文</el-divider>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.apply{
+  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1)
+}
+</style>
+
