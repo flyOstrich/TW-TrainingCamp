ca199f6f8001908a9faff37356f746a689ec5b89
jiawei.chen <jiawei.chen@thoughtworks.com>
2019-06-02 09:04:54 +0800

    1) add cros-region support. 2) format response with json.

--- backend/camp/src/main/java/com/firefly/training/camp/domain/rest/UserResource.java
+++ backend/camp/src/main/java/com/firefly/training/camp/domain/rest/UserResource.java
@@ -1,5 +1,6 @@
 package com.firefly.training.camp.domain.rest;
 
+import com.firefly.training.camp.domain.dto.UserCheckDTO;
 import com.firefly.training.camp.domain.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,9 +16,9 @@ public class UserResource {
     private UserService userService;
 
     @GetMapping
-    public Boolean checkUser(
+    public UserCheckDTO checkUser(
             @RequestParam String userName,
             @RequestParam String pwd) {
-        return userService.checkUser(userName, pwd);
+        return new UserCheckDTO(userService.checkUser(userName, pwd));
     }
 }
